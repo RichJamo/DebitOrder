3d0043960d499c8b37735c5aa4415d5d0d1ab076
Richard Jamieson <jamieson.rich@gmail.com>
2022-10-11 13:22:36 +0200

    fixed test and dependencies

--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -2,23 +2,23 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
-import "../src/Counter.sol";
+import "../src/DebitOrder.sol";
 
 contract CounterTest is Test {
-    Counter public counter;
+    DebitOrder public debitOrder;
 
     function setUp() public {
-        counter = new Counter();
-        counter.setNumber(0);
+        debitOrder = new DebitOrder();
+        // debitOrder.setNumber(0);
     }
 
-    function testIncrement() public {
-        counter.increment();
-        assertEq(counter.number(), 1);
-    }
+    // function testIncrement() public {
+    //     debitOrder.increment();
+    //     assertEq(debitOrder.number(), 1);
+    // }
 
-    function testSetNumber(uint256 x) public {
-        counter.setNumber(x);
-        assertEq(counter.number(), x);
-    }
+    // function testSetNumber(uint256 x) public {
+    //     debitOrder.setNumber(x);
+    //     assertEq(debitOrder.number(), x);
+    // }
 }
